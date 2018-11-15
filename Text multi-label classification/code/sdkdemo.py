a455a0ab3907e4e6d2eb6c2fdf259c71e2abf3ea
chenjing34 <chenjing34@lenovo.com>
2018-11-15 23:54:57 +0800

    Add new file

---
+++ Text multi-label classification/code/sdkdemo.py
@@ -0,0 +1,25 @@
+from solution import Solution
+from mlstudiosdk.modules.scheme.mode import Mode
+
+solution = Solution()
+
+params = {
+    'train input':{
+        'datapath': 'fasttext_training_1115.csv',
+        'label': 'label'
+    },
+    'test input':{
+        'datapath': 'fasttext_test_1115.csv',
+        'label': 'label'
+    },
+    'algorithm':{
+
+    },
+    'output':{
+        "filename": 'a.csv',
+    }
+}
+
+solution.set_params(params)
+solution.run()
+solution.run(Mode.Test)
