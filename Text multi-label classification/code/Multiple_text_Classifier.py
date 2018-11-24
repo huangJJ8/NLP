688f6d21eb9c98a21b436973209804208fe1213e
chenjing34 <chenjing34@lenovo.com>
2018-11-24 18:26:49 +0800

    Update Multiple_text_Classifier.py

--- Text multi-label classification/code/Multiple_text_Classifier.py
+++ Text multi-label classification/code/Multiple_text_Classifier.py
@@ -230,7 +230,7 @@ class Multiple_text_Classifier(LComponent):
         self.text_split(validationa,'validationset')       
         #training the model
         self.classifier=fasttext.supervised(self.file_path+'/fasttext_trainingset.txt',self.file_path+'/fasttext_test.model',label_prefix='__label__',
-                                       epoch=200,min_count=3,word_ngrams=3,bucket=1,ws=15)
+                                       epoch=200,min_count=3,word_ngrams=3,ws=10,thread=32,lr=0.1,dim=200,bucket=5000000)
       
         train_result=self.classifier.test(self.file_path+'/fasttext_trainingset.txt')
         train_score=train_result.precision
